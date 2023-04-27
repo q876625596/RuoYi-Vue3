a1c47478eb873add3cdd9f6472ac8bec486065ca
kumi <876625596@qq.com>
2023-04-27 18:24:15 +0800

    修改

--- src/utils/request.js
+++ src/utils/request.js
@@ -30,7 +30,6 @@ service.interceptors.request.use(config => {
         refreshToken();
         config.headers['token'] = 'Bearer ' + getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
     }
-    config.headers['deviceid'] = piniaStore.userStore.deviceId
     // get请求映射params参数
     if (config.method === 'get' && config.params) {
         let url = config.url + '?' + tansParams(config.params);
